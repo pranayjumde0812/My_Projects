ba4d247b99c5f184dbbaba7e6ef5a418db0e776e
Pranay Jumde <pranayjumde13@gmail.com>
2023-05-29 15:48:29 +0530

    Modification in registration form

--- trading/src/main/java/com/mkpits/trading/service/RegisterTradingDataService.java
+++ trading/src/main/java/com/mkpits/trading/service/RegisterTradingDataService.java
@@ -3,6 +3,6 @@ package com.mkpits.trading.service;
 import com.mkpits.trading.dto.response.TradingDataDto;
 
 
-public interface RegisterTradingDataService {
+public interface  RegisterTradingDataService {
     public TradingDataDto getRegistrationData(TradingDataDto tradingDataDto);
 }
--- trading/src/main/java/com/mkpits/trading/service/impl/TradingRegServiceImpl.java
+++ trading/src/main/java/com/mkpits/trading/service/impl/TradingRegServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 @Service
-@Primary
+
 public class TradingRegServiceImpl implements TradingRegService {
 
     @Autowired
